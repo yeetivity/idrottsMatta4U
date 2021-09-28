19a1f3dcd4ba93d3de84619641b07ede9fd014e7
Jitse vE <jj.ve.student@gmail.com>
2021-09-28 16:14:56 +0200

    Added function statements for the filters

---
+++ data_process.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class DataProcess(object):
+    """
+    
+    """
+
+    def combineAccelerations():
+        """
+        Function that processes the accelerations
+        """
+
+        return
+
+    def simpelKalmanFilter():
+        """
+        Kalman with one dimension
+        """
+
+        return
+
+    def complexKalmanFilter():
+        """
+        Kalman with multiple dimensions
+        """
+
+        return
+    
+    def filter():
+        """
+        If you don't want to sleep, make EMWA filter
+        """
+
+        return
