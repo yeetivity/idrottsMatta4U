67c49273dd1f2ec640a6fa39fd44423d156e5902
Jitse vE <jj.ve.student@gmail.com>
2021-10-06 16:33:04 +0200

    Added a class for the settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings(object):
+    """
+    =NOTES=
+    """
+
+    #Run settings
+    experiment = 3
+
+    #Plot settings
+    padding = 5
+    
+    #Physical settings
+    gravity = 9.81
+
+    
